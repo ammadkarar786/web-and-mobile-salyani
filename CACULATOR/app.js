f798b60786987e9c27add4c802070c9683df3b15
ammadkarar <ammadkararuni@gmail.com>
2020-06-29 07:57:49 +0500

    done

--- CACULATOR/app.js
+++ CACULATOR/app.js
@@ -1,2 +1,15 @@
+function getNumber(num){
+    var result= document.getElementById("answer")
+    result.value+=num;
 
+}
+function getClear(){
+    var result= document.getElementById("answer")
+    result.value="";
 
+}
+
+function getResult(){
+    var result = document.getElementById("answer");
+    result.value=eval(result.value)
+}
